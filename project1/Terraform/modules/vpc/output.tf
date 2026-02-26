ef3091c3fefd3186720956ef825a5896f287c993
Kiran <kiranpithani999@gmail.com>
2026-02-26 13:10:35 +0530

    change15

--- project1/Terraform/modules/vpc/output.tf
+++ project1/Terraform/modules/vpc/output.tf
@@ -9,3 +9,11 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
+
+output "nat_gateway_id" {
+  value = aws_nat_gateway.nat_gw.id
+}
+
+output "elastic_ip" {
+  value = aws_eip.nat_eip.public_ip
+}
