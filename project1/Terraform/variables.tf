f9c278291ca72cb950f44c8dd6121fdd804b083f
Kiran <kiranpithani999@gmail.com>
2026-02-26 12:50:52 +0530

    change12

--- project1/Terraform/variables.tf
+++ project1/Terraform/variables.tf
@@ -1,7 +1,7 @@
 # ----------------------------
 # AWS REGION
 # ----------------------------
-variable "region" {
+variable "aws_region" {
   description = "AWS Region"
   type        = string
   default     = "ap-south-1"
